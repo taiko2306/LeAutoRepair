3a2bc6341f0b62ed5b02c9800255c695570c63d5
Taiko2306 <taiko.aronson@gmail.com>
2020-05-30 11:21:30 -0700

    fix image detail

--- main/views.py
+++ main/views.py
@@ -85,24 +85,11 @@ def UpdateVehicle(request, pk):
         form = VehicleForm(request.POST or None, request.FILES, instance=vehicle)
 
         if form.is_valid():
-            #vehicle.vin = form.cleaned_data["vin"]
-            #vehicle.reg_number = form.cleaned_data["reg_number"]
-            #vehicle.make = form.cleaned_data["make"]
-            #vehicle.model = form.cleaned_data["model"]
-            #vehicle.year = form.cleaned_data["year"]
-            #vehicle.color = form.cleaned_data["color"]
-            #if 'image' in request.FILES:
-            #    vehicle.image = form.cleaned_data["image"]
             form.save()
             #vehicle.save()
-
-            print("Save form:" + str(vehicle))
-            print(vehicle)
-            print(vehicle.imageURL)
             return HttpResponseRedirect(reverse_lazy('vehicle-list-view'))
 
     else:
-        print('REQUEST')
         form = VehicleForm(initial=model_to_dict(vehicle))
 
     context = {'operation': 'Modify', 'form': form}
